51fda4a5910c71df266ef9d10a196fc8ab629d5a
webmucidi <fatihcelik1907@gmail.com>
2021-04-03 02:41:41 +0300

    ilk yukleme

---
+++ script.js
@@ -0,0 +1,62 @@
+function oyunuBaslat(secim){
+    //Önce kullanıcın seçimi değişkene aktaralım
+    let kullaniciSecimi=secim.id;
+    console.log(kullaniciSecimi);
+    //Bilgisayar tarafından oluşturulan rastgele seçimi değişkene aktaralım
+    let rastgeleSayi=Math.floor(Math.random()*3);
+    let bilgisayarSecimi=["tas","kagit","makas"][rastgeleSayi];
+    console.log(bilgisayarSecimi);
+    //Puanlamaları dizi şeklinde değişkene aktaralım
+    let oyunPuanlamalari={
+        "tas":{"makas":1,"tas":0.5,"kagit":0},
+        "kagit":{"tas":1,"kagit":0.5,"makas":0},
+        "makas":{"kagit":1,"makas":0.5,"tas":0}
+    };
+
+    //Diziden seçimimize karşılık gelen puanı alalım
+    let kullaniciPuani=oyunPuanlamalari[kullaniciSecimi][bilgisayarSecimi];
+    console.log(kullaniciPuani);
+
+    //Kayıtlı tüm resimlerin kaynak adreslerini alalım
+    let kayitliResimler={
+        "tas":document.getElementById("tas").src,
+        "kagit":document.getElementById("kagit").src,
+        "makas":document.getElementById("makas").src
+    };
+
+    //Temiz bir sonuç ekranı için tüm resimleri silelim
+    document.getElementById("tas").remove();
+    document.getElementById("kagit").remove();
+    document.getElementById("makas").remove();
+
+    //Sildiğimiz nesnelerin yerine sonuçlar için yeni nesneler oluşturalım
+    let kullaniciResmi=document.createElement("img");
+    let bilgisayarResmi=document.createElement("img");
+    let sonucMesaji=document.createElement("div");
+
+    //Resim nesnelerine seçimlere göre uygun kaynakları bağlayalım
+    kullaniciResmi.src=kayitliResimler[kullaniciSecimi];
+    bilgisayarResmi.src=kayitliResimler[bilgisayarSecimi];
+
+    //Oluşturduğumuz nesneleri kapsayıcısı altına monte edelim
+    document.getElementById("container").appendChild(kullaniciResmi);
+    document.getElementById("container").appendChild(sonucMesaji);
+    document.getElementById("container").appendChild(bilgisayarResmi);
+
+    //Puana bakarak şart kontrolüyle sonucu yazdıralım
+    sonucMesaji.classList.remove("basarili","basarisiz","berabere");
+
+    if(kullaniciPuani === 0){
+        sonucMesaji.innerHTML="Maalesef kaybettiniz :(";
+        sonucMesaji.classList.add("basarisiz");
+    }
+    else if(kullaniciPuani === 0.5){
+        sonucMesaji.innerHTML="Sonuç berabere";
+        sonucMesaji.classList.add("berabere");
+    }
+    else{
+        sonucMesaji.innerHTML="Tebrikler kazandınız :)";
+        sonucMesaji.classList.add("basarili");
+    }
+
+}
